8752c8dee5cf2a371349954f8e49c8fd2cc0a92a
adamKonyari <adam.konyari@gmail.com>
2019-02-14 14:58:07 +0100

    base setup

---
+++ src/main/java/com/codecool/Main.java
@@ -0,0 +1,11 @@
+package com.codecool;
+
+public class Main {
+
+    public static void Main(String[] args) {
+
+        UserInterface userInterface = new UserInterface();
+
+    }
+
+}
---
+++ src/main/java/com/codecool/api/Dealership.java
@@ -0,0 +1,7 @@
+package com.codecool.api;
+
+public class Dealership {
+
+    private String name;
+    
+}
